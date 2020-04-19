a95f4121bc3176af65d79a43d6e766a5a4b4c2e3
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-19 18:57:49 -0500

    added functionality to get profile data

--- photo-app/src/components/UserProfile.js
+++ photo-app/src/components/UserProfile.js
@@ -1,22 +1,35 @@
 import React ,{Component} from 'react'
-import {storage} from '../config/fireauth'
+import firebase,{storage} from '../config/fireauth'
 
 class UserProfile extends Component{
     constructor(props){
         super(props)
         this.state={
             file:'',
-            url:''
+            url:'',
+            user:[],
+            isLoaded:false
+
         }
         this.downloadImages = this.downloadImages.bind(this)
+        this.getProfile = this.getProfile.bind(this)
 
     }
     componentDidMount(){
-        console.log("inside component didmount")
+        console.log("inside component UserProfile didmount")
+        this.getProfile()
         this.downloadImages();
     }
+    getProfile(){
+        const user = firebase.auth().currentUser;
+        if (user != null) {
+            this.state.user.push({ displayName:user.displayName,email:user.email,photoURL:user.photoURL,uid:user.uid})
+            this.setState({isLoaded:true})
+        }
+        console.log("this.state.user is:", this.state.user)
+    }
     downloadImages(){
-        console.log("inside component didmount")
+        console.log("inside download image")
         const storageRef = storage.ref();
         var starsRef = storageRef.child('images/connect4-red-image1.png');
         // Get the download URL
@@ -31,28 +44,42 @@ class UserProfile extends Component{
             case 'storage/object-not-found':
             // File doesn't exist
             break;
-
             case 'storage/unauthorized':
             // User doesn't have permission to access the object
             break;
-
             case 'storage/canceled':
             // User canceled the upload
             break;
-
-
             case 'storage/unknown':
             // Unknown error occurred, inspect the server response
             break;
+            default:
+                break;
         }
 });
     }
     render(){
+        if(this.state.isLoaded===true)
         return (
             <div className="main">
-                       Home
+                <div className="image">
+                    <img src={this.state.user.photoURL} width="150px" height="150px"></img>
+                </div>
+                <div className="profile">
+                        <h5>Name :{this.state.user[0].displayName}</h5>
+                        <h5>Email :{this.state.user[0].email}</h5>
+                        <h5>UID :{this.state.user[0].uid}</h5>
+
+                </div>
              </div>
         )
+        else{
+            return (
+                <div className="main">
+                
+                 </div>
+            )            
+        }
     }
  
 }
