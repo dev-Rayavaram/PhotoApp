bddcf418d7eeb1b1c1071629c0d7bb500ea5797e
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-18 20:13:03 -0500

    added navbar components

--- photo-app/src/App.js
+++ photo-app/src/App.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
-import './App.css';
+import './App.scss';
 import fireauth from './config/fireauth'
 import Login from './components/Login'
 import Home from './components/Home'
+import Bookmarks from './components/Bookmarks'
+import UserProfile from './components/UserProfile'
+import Logout from './components/Logout'
+
+import {Route,Switch,Link,BrowserRouter as Router} from 'react-router-dom'
+import axios from 'axios';
+
 
 class  App extends Component {
   constructor(props){
@@ -31,7 +38,39 @@ class  App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {this.state.user ? (<Home/>):(<Login/>)}
+          {this.state.user ? 
+          (
+                <Router>  
+                <div >
+                  <nav>
+                      <ul>
+                      <li>
+                        <Link to="/" >Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/UserProfile">User Profile</Link>
+                      </li>
+                      <li>
+                        <Link to="/Bookmarks">Bookmarks</Link>
+                      </li>
+                      <li>
+                        <Link to="/Logout" >Logout</Link>
+                      </li>
+                      </ul>
+                  </nav>
+                  <Switch>
+                       <Route exact path="/" component={Home}>          
+                      </Route> 
+                      <Route exact path="/UserProfile" component={UserProfile}>          
+                      </Route> 
+                      <Route exact path="/Bookmarks" component={Bookmarks}>          
+                      </Route> 
+                      <Route exact path="/Logout" component={Logout}>          
+                      </Route>             
+                  </Switch>
+                </div>
+                </Router>            
+          ):(<Login/>)}
          </header>
       </div>
     );
