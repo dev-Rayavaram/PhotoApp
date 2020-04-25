fac9b7f2bc966afade749125817dc76866b27c8a
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-25 16:17:35 -0500

    added bookmarks functionality

--- photo-app/src/components/Bookmarks.js
+++ photo-app/src/components/Bookmarks.js
@@ -1,12 +1,41 @@
 import React ,{Component} from 'react'
 
+
 class Bookmarks extends Component{
     render(){
-        return (
-            <div className="page">
-                       Bookmarks
-             </div>
-        )
+        if(this.props && this.props.images && this.props.images !== undefined){
+            return (
+                <div className="main">
+                   <div className="container">
+                   <div className="sub-container-2">
+                            {
+                                this.props.images.map((image,index)=>{return(
+                                    <React.Fragment>
+                                        <>
+                                        <caption>Likes:{image.liked}</caption>
+    
+                                            <img src={image.image} alt="main" style={{width:"250px",height:"250px"}} ></img>
+                                        </>
+                                        </React.Fragment>
+                                )})
+                            }
+                        </div>
+                   </div>
+                 </div>
+            )
+
+        }
+        else{
+            return(
+                    <div className="main">
+                        <div className="container">
+                        <div className="sub-container-2">
+                         </div>
+                    </div>
+                 </div>
+
+            )
+        }
     }
  
 }
