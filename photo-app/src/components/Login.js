0832fdffbceee4994c876d39bfcb80c2848b6ec0
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-26 10:31:14 -0500

    removed html for from login

--- photo-app/src/components/Login.js
+++ photo-app/src/components/Login.js
@@ -59,12 +59,12 @@ class  Login extends Component {
         <div className="container">
             <form>
                 <div className="col-8">
-                    <label className="label" for= "EmailId">Email:</label>
+                    <label className="label" >Email:</label>
                     <input  className="input" value={this.state.email} onChange={this.handleChange} type="email" name="email"
                     id="EmailId" />
                 </div>
                 <div className="col-8">
-                    <label for="PasswordId">Password:</label>
+                    <label >Password:</label>
                     <input className="input"  value={this.state.password} onChange={this.handleChange} type="password" name="password"
                     id="PasswordId" />
                 </div>
